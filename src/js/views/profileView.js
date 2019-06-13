c8dd08f4b821d003eb828350e4c37d3ba4e9c55b
Andre Landgraf <anla@netlight.com>
2019-06-13 19:44:29 +0200

    lifestyles and goals

--- src/js/views/profileView.js
+++ src/js/views/profileView.js
@@ -4,6 +4,8 @@ import i18n from 'i18next';
 
 import { KEYS } from '../utilities/internationalization/internationalization';
 
+import GoalsContainer from '../container/Goals/GoalsContainer';
+import LifestylesContainer from '../container/Lifestyles/LifestylesContainer';
 import DislikesContainer from '../container/Dislikes/DislikesContainer';
 import AllergiesContainer from '../container/Allergies/AllergiesContainer';
 
@@ -11,6 +13,8 @@ import AllergiesContainer from '../container/Allergies/AllergiesContainer';
 const ProfileView = ( { user } ) => (
     <div className="container profile-container">
         <h1>{ i18n.t( KEYS.HEADERS.PROFILE_TITLE ) }</h1>
+        <GoalsContainer user={user} />
+        <LifestylesContainer user={user} />
         <DislikesContainer user={user} />
         <AllergiesContainer user={user} />
     </div>
