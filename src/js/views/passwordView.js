f5c1f13b962fcb9730bcea35ca37147db1a99a2e
Andre Landgraf <andre.timo.landgraf@gmail.com>
2019-06-20 00:39:24 +0200

    added succ message and styled box a bit

--- src/js/views/passwordView.js
+++ src/js/views/passwordView.js
@@ -4,7 +4,7 @@ import PasswordContainer from '../container/Password/PasswordContainer';
 
 // eslint-disable-next-line no-unused-vars
 const PasswordView = () => (
-    <div className="container home-container">
+    <div className="container password-container">
         <h1>Change Password</h1>
         <PasswordContainer />
     </div>
