0299fe8b8f02a670e4f3b97909a6d5308a028312
Marcus <marcus.land@tum.de>
2019-06-20 23:53:03 +0200

    first draft of the detail view

--- src/js/components/recipe/recipe.js
+++ src/js/components/recipe/recipe.js
@@ -3,14 +3,33 @@ import PropTypes from 'prop-types';
 
 const Recipe = ( { recipe } ) => (
     <div className="recipe">
-        <h2>{ recipe.name }</h2>
-        { recipe.ingredients.map(
-            ingderient => (
-                <div key={ingderient._id} className="ingridient">
-                    { ingderient.label._id }
-                </div>
-            ),
-        )}
+        <h1>{ recipe.name }</h1>
+        <i>{recipe.category}</i>
+        <img src={recipe.imgUrl} alt={recipe.name} className="recipePic" />
+        <div>
+            <img
+                src="../../img/hourglass.svg"
+                alt="Preparation time"
+                classes="icon-item"
+            />
+            {' : '}
+            {recipe.preparationTime}
+            min
+        </div>
+        <div>
+    Servings:
+            {' '}
+            <input type="number" id="servings" min="1" max="100" step="1" value={recipe.servings} />
+        </div>
+        <ul>
+            { recipe.ingredients.map(
+                ingredient => (
+                    <li key={ingredient._id}>
+                        { ingredient.label.label}
+                    </li>
+                ),
+            )}
+        </ul>
     </div>
 );
 
@@ -18,12 +37,15 @@ Recipe.propTypes = {
     recipe: PropTypes.shape( {
         _id: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
+        preparationTime: PropTypes.number.isRequired,
+        meal: PropTypes.string.isRequired,
         ingredients: PropTypes.arrayOf(
             PropTypes.shape( {
                 label: PropTypes.string.isRequired,
                 _id: PropTypes.string.isRequired,
             } ),
         ).isRequired,
+        imgUrl: PropTypes.string,
     } ).isRequired,
 };
 
