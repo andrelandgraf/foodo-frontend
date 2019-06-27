150b36be20332f408c00b21b6d75a2cca3624f3a
andrelandgraf <andre.timo.landgraf@gmail.com>
2019-06-27 12:11:27 +0200

    minor fix

--- src/js/utilities/redirect.js
+++ src/js/utilities/redirect.js
@@ -1,8 +1,8 @@
-import { NONAUTH_ROUTES } from '../container/App/App';
+import { NONAUTH_ROUTES, AUTH_ROUTES } from '../container/App/App';
 
 export const setRedirectUrl = url => window.sessionStorage.setItem( 'redirectUrl', url );
 
-export const getRedirectUrl = () => window.sessionStorage.getItem( 'redirectUrl' );
+export const getRedirectUrl = () => window.sessionStorage.getItem( 'redirectUrl' ) || AUTH_ROUTES.HOME;
 
 export const isValidRedirectUrl = path => !path.startsWith( NONAUTH_ROUTES.LOGIN )
                                     && !path.startsWith( NONAUTH_ROUTES.Reg );
