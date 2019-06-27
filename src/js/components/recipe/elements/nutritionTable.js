7d39ab69be552f587f97764ba758c557993e3099
Marcus <marcus.land@tum.de>
2019-06-27 12:29:30 +0200

    Add new rows to nutri table

--- src/js/components/recipe/elements/nutritionTable.js
+++ src/js/components/recipe/elements/nutritionTable.js
@@ -5,7 +5,7 @@ import i18n from 'i18next';
 
 import { KEYS } from '../../../utilities/internationalization/internationalization';
 
-
+// %- reference based on https://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=OJ:L:2011:304:0018:0063:de:PDF
 const NutritionTable = ( { ingredients } ) => {
     const totalWeight = ingredients.map(
         ingredient => ( ingredient.amount * ingredient.unit.amount ),
@@ -15,6 +15,18 @@ const NutritionTable = ( { ingredients } ) => {
     ).reduce( ( pv, cv ) => pv + cv, 0 ).toFixed( 1 );
     const totalFat = ingredients.map(
         ingredient => ( ingredient.elements.TotalFat * ingredient.amount ),
+    ).reduce( ( pv, cv ) => pv + cv, 0 ).toFixed( 1 );
+    const totalSFA = ingredients.map(
+        ingredient => ( ingredient.elements.SFA * ingredient.amount ),
+    ).reduce( ( pv, cv ) => pv + cv, 0 ).toFixed( 1 );
+    const totalCarbs = ingredients.map(
+        ingredient => ( ingredient.elements.Carbohydrate * ingredient.amount ),
+    ).reduce( ( pv, cv ) => pv + cv, 0 ).toFixed( 1 );
+    const totalSugar = ingredients.map(
+        ingredient => ( ingredient.elements.AddedSugars * ingredient.amount ),
+    ).reduce( ( pv, cv ) => pv + cv, 0 ).toFixed( 1 );
+    const totalFiber = ingredients.map(
+        ingredient => ( ingredient.elements.DietaryFiber * ingredient.amount ),
     ).reduce( ( pv, cv ) => pv + cv, 0 ).toFixed( 1 );
     const totalProtein = ingredients.map(
         ingredient => ( ingredient.elements.Protein * ingredient.amount ),
@@ -73,6 +85,79 @@ g
                             {( ( totalFat / totalWeight * 400 ) * 100 / 70 ).toFixed( 0 )}
 %
                         </td>
+                    </tr>
+                    <tr>
+                        <td>Saturated</td>
+                        <td>
+                            {totalSFA}
+g
+                        </td>
+                        <td>
+                            {( totalSFA / totalWeight * 100 ).toFixed( 1 )}
+g
+                        </td>
+                        <td>
+                            {( totalSFA / totalWeight * 400 ).toFixed( 1 )}
+g
+                        </td>
+                        <td>
+                            {( ( totalSFA / totalWeight * 400 ) * 100 / 20 ).toFixed( 0 )}
+%
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Carbohydrates</td>
+                        <td>
+                            {totalCarbs}
+g
+                        </td>
+                        <td>
+                            {( totalCarbs / totalWeight * 100 ).toFixed( 1 )}
+g
+                        </td>
+                        <td>
+                            {( totalCarbs / totalWeight * 400 ).toFixed( 1 )}
+g
+                        </td>
+                        <td>
+                            {( ( totalCarbs / totalWeight * 400 ) * 100 / 260 ).toFixed( 0 )}
+%
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Sugar</td>
+                        <td>
+                            {totalSugar}
+g
+                        </td>
+                        <td>
+                            {( totalSugar / totalWeight * 100 ).toFixed( 1 )}
+g
+                        </td>
+                        <td>
+                            {( totalSugar / totalWeight * 400 ).toFixed( 1 )}
+g
+                        </td>
+                        <td>
+                            {( ( totalSugar / totalWeight * 400 ) * 100 / 90 ).toFixed( 0 )}
+%
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Fiber</td>
+                        <td>
+                            {totalFiber}
+g
+                        </td>
+                        <td>
+                            {( totalFiber / totalWeight * 100 ).toFixed( 1 )}
+g
+                        </td>
+                        <td>
+                            {( totalFiber / totalWeight * 400 ).toFixed( 1 )}
+g
+                        </td>
+                        <td />
                     </tr>
                     <tr>
                         <td>Proteins</td>
