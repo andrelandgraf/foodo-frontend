e97b9b8e752ec495083b0638c160d439b626e059
andrelandgraf <andre.timo.landgraf@gmail.com>
2019-07-02 19:19:23 +0200

    fixed charts on mobile

--- src/js/hooks/useDeviceState.js
+++ src/js/hooks/useDeviceState.js
@@ -4,11 +4,13 @@ const MOBILE_WIDTH_BREAKPOINT = 750;
 
 const useDeviceState = () => {
     const [ isMobile, setIsMobile ] = useState( MOBILE_WIDTH_BREAKPOINT >= window.innerWidth );
+    const [ innerWidth, setInnerWidth ] = useState( window.innerWidth );
 
     useEffect( () => {
         const handleResize = () => {
             const widthIsMobile = MOBILE_WIDTH_BREAKPOINT >= window.innerWidth;
             setIsMobile( widthIsMobile );
+            setInnerWidth( window.innerWidth );
         };
 
         window.addEventListener( 'resize', handleResize );
@@ -16,7 +18,7 @@ const useDeviceState = () => {
         return () => window.removeEventListener( 'resize', handleResize );
     }, [] );
 
-    return isMobile;
+    return [ isMobile, innerWidth ];
 };
 
 export default useDeviceState;
