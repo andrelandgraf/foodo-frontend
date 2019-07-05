9427f25d7ba93592fe84cf83525195002bfa8ecd
andrelandgraf <andre.timo.landgraf@gmail.com>
2019-07-05 14:51:05 +0200

    implemented boxgrids

---
+++ src/js/components/boxgrid/boxgrid.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Loader from '../loading/loader';
+import Box from '../box/box';
+
+function Boxgrid( { recipes, title, onClick } ) {
+    return (
+        <div className="box-view">
+            { title }
+            { recipes
+                ? (
+                    <div className="box-view-grid">
+                        { recipes.map( r => <Box key={r.key} recipe={r} onClick={onClick} /> )}
+                    </div>
+                )
+                : <Loader key="lodaing-recipes" />
+            }
+        </div>
+    );
+}
+
+Boxgrid.propTypes = {
+    recipes: PropTypes.arrayOf(
+        PropTypes.shape( {
+            label: PropTypes.string.isRequired,
+            key: PropTypes.string.isRequired,
+        } ).isRequired,
+    ),
+    title: PropTypes.oneOfType( [
+        PropTypes.string,
+        PropTypes.node,
+    ] ).isRequired,
+    onClick: PropTypes.func.isRequired,
+};
+
+Boxgrid.defaultProps = {
+    recipes: undefined,
+};
+
+export default Boxgrid;
