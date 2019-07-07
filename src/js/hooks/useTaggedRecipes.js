6684d1606b06601d7bba73b39ceeecd950d47cb0
andrelandgraf <andre.timo.landgraf@gmail.com>
2019-07-07 22:28:17 +0200

    added hook to add tags to recipes

---
+++ src/js/hooks/useTaggedRecipes.js
@@ -0,0 +1,54 @@
+import { useState, useContext, useEffect } from 'react';
+
+import { IngredientsContext } from '../provider/IngredientsProvider';
+import { UserStateContext } from '../provider/UserStateProvider';
+import useDisplayableRecipes from './useDisplayableRecipes';
+
+
+const getIngredientAllergies = ( ingredient, ingredients ) => ingredients
+    .find( i => i._id === ingredient.ingredient ).notForAllergy;
+
+const getIngredientNotLifestyles = ( ingredient, ingredients ) => ingredients
+    .find( i => i._id === ingredient.ingredient ).notForLifestyles;
+
+const getNotForAllergies = ( recipe, ingredients, allergies ) => allergies
+    .filter( a => recipe.ingredients
+        .find( i => getIngredientAllergies( i, ingredients )
+            .find( ia => ia === a._id ) ) )
+    .map( a => a.name );
+
+const getNotForLifestyles = ( recipe, ingredients, lifestyle ) => (
+    recipe.ingredients
+        .filter( i => getIngredientNotLifestyles( i, ingredients )
+            .find( l => l === lifestyle._id ) )
+        .length
+        ? lifestyle.name
+        : ''
+);
+
+const mapRecipesTagged = ( recipes, ingredients, user ) => (
+    recipes && ingredients && ingredients.length && user
+        ? recipes.map( recipe => ( {
+            ...recipe,
+            notForAllergies: getNotForAllergies( recipe, ingredients, user.allergies ),
+            notForLifestyles: getNotForLifestyles( recipe, ingredients, user.lifestyle ),
+        } ) )
+        : recipes
+);
+
+function useTaggedRecipes() {
+    const displayableRecipes = useDisplayableRecipes();
+    const { ingredients } = useContext( IngredientsContext );
+    const { user } = useContext( UserStateContext );
+    const [ taggedRecipes, setTaggedRecipes ] = useState(
+        mapRecipesTagged( displayableRecipes, ingredients, user ),
+    );
+
+    useEffect( () => {
+        setTaggedRecipes( mapRecipesTagged( displayableRecipes, ingredients, user ) );
+    }, [ displayableRecipes, ingredients, user ] );
+
+    return taggedRecipes;
+}
+
+export default useTaggedRecipes;
