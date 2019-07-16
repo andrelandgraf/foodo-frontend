cbdf49e52ac71590a8ca611ffd1e1d9e6e826374
andrelandgraf <andre.timo.landgraf@gmail.com>
2019-07-17 00:50:55 +0200

    party

--- src/js/views/homeView.js
+++ src/js/views/homeView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import i18n from 'i18next';
 
@@ -7,14 +7,52 @@ import { KEYS } from '../utilities/internationalization/internationalization';
 import RecipesContainer from '../container/Recipes/RecipesContainer';
 import RecipesGridsContainer from '../container/Recipes/RecipesGridsContainer';
 
-const HomeView = ( { user } ) => (
-    <div className="container home-container">
-        <h1>{i18n.t( KEYS.HEADERS.HOME_WELCOME, { name: user.username } )}</h1>
-        <RecipesContainer />
-        <RecipesGridsContainer />
-    </div>
+function dancingText() {
+    const h1 = document.querySelector( '.header' );
+    const text = h1.textContent;
+    const array = text.split( '' );
+    let innerHTML = '';
+    array.forEach( ( value ) => {
+        innerHTML += `<span class='dancing-text'>${ value }</span>`;
+    } );
+    h1.innerHTML = innerHTML;
 
-);
+    function dance( i ) {
+        const textBoxes = document.querySelectorAll( '.dancing-text' );
+        const { length } = textBoxes;
+        if ( i >= length ) {
+            textBoxes[ i - 1 ].classList.toggle( 'funky' );
+            h1.classList.toggle( 'bright-shadow' );
+            return;
+        }
+        textBoxes[ i ].classList.toggle( 'funky' );
+        if ( i > 0 ) {
+            textBoxes[ i - 1 ].classList.toggle( 'funky' );
+        } else {
+            h1.classList.toggle( 'bright-shadow' );
+        }
+        setTimeout( () => {
+            dance( i + 1 );
+        }, 250 );
+    }
+
+    setInterval( () => {
+        dance( 0 );
+    }, 5000 );
+}
+
+const HomeView = ( { user } ) => {
+    useEffect( () => { dancingText(); }, [] );
+
+    return (
+        <div className="container home-container">
+            <h1 className="header">{i18n.t( KEYS.HEADERS.HOME_WELCOME, { name: user.username } )}</h1>
+            <RecipesContainer />
+            <RecipesGridsContainer />
+        </div>
+
+    );
+};
 
 HomeView.propTypes = {
     user: PropTypes.shape( {
