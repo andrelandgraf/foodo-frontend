054bb8634f16efe7a2e1fd3adb988e7c634bc030
andrelandgraf <andre.timo.landgraf@gmail.com>
2019-07-27 23:00:06 +0200

    notfound view refactored

--- src/js/components/view/notFoundView.js
+++ src/js/components/view/notFoundView.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import i18n from 'i18next';
 
-import { KEYS } from '../utilities/internationalization/internationalization';
+import { KEYS } from '../../utilities/internationalization/internationalization';
+
+import SimpleView from './simpleView';
 
 const NotFoundView = () => (
-    <div className="container">
+    <SimpleView title={<h1>{i18n.t( KEYS.HEADERS.PAGE_NOT_FOUND )}</h1>}>
         <div className="center">
-            <h1>{i18n.t( KEYS.HEADERS.PAGE_NOT_FOUND )}</h1>
             {i18n.t( KEYS.MESSAGES.PAGE_NOT_FOUND_MESSAGE )}
         </div>
-    </div>
+    </SimpleView>
 );
 
 export default NotFoundView;
